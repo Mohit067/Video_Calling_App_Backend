d2fc780cbc42e07e7bd1f05255823d28206968bb
Mohit sahu <mohitsahu60067@gmail.com>
2025-02-26 15:02:27 +0530

    Handles room creation and user join with record storage.

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -1,16 +1,35 @@
 import { Socket } from 'socket.io';
 import { v4 as UUIDv4} from 'uuid';
+import RoomsParams from '../interfaces/RoomParams';
+
+const rooms: Record<string, string[]> = {};
 
 export const roomHandler = ( socket: Socket ) => {
+
+    
     const createRoom = () => {
-        const roomId = UUIDv4(); //
+        const roomId = UUIDv4();
+
         socket.join(roomId); // socket connection for entering a new room
+        rooms[roomId] = []; // create the new entery for the room
         socket.emit('room-created', { roomId }); // here we'll emit an event
         console.log('room connected with id : ', roomId);
     };
 
-    const joinedRoom = ({ roomId }: { roomId: string}) => {
-        console.log('new user joined in room : ', roomId);
+    const joinedRoom = ({ roomId, peerId }: RoomsParams) => {
+        console.log('joined room called', rooms);
+
+        if (rooms[roomId]) {
+            console.log('new user joined in room : ', roomId, 'with peerId is', peerId);
+            rooms[roomId].push(peerId);
+            console.log('added peer to room', rooms);
+            socket.join(roomId);
+        }
+
+        socket.emit('get-users', {
+            roomId,
+            participants: rooms[roomId],
+        });
     };
 
     socket.on('create-room', createRoom);
---
+++ src/interfaces/RoomParams.ts
@@ -0,0 +1,4 @@
+export default interface RoomsParams {
+    roomId: string;
+    peerId: string;
+}
